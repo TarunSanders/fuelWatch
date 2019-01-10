8ee7bdaf5fdc08f15eb0536a27d16c91985872a4
TarunSanders <sandtm1703@gmail.com.au>
2019-01-10 19:09:15 +0800

    created own dictionary

--- fuelwatch2.py
+++ fuelwatch2.py
@@ -1,16 +1,25 @@
 #import requests
 #r = requests.get('https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS')
 import feedparser
-from datetime import datetime
+#from datetime import datetime
 from pprint import pprint 
 #
-def get_fuel(suburbAndSurrounding,day): #gets list of dictionaries with fuel info and returns this data structure
+def get_fuel(suburbAndSurrounding,day): #gets list of dictionaries with fuel info and returns this data in own dictionary with imp info
 	product_id = 1; #1: unleaded
 	url = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={prod}&Suburb={sub}&Day={D}' # product = ? query strings
 	url = url.format(prod = str(product_id), sub = suburbAndSurrounding, D = day) #.format is not mutable function/method
 	#print(url)
 	data = feedparser.parse(url) #grabs RSS data from url and converts RSS to dictionary format if not already
-	return(data['entries'])
+	
+	dataImp = [ {'price': entry['price'], 
+				'location': entry['location'],
+				'brand':entry['brand'], 
+				'date': entry['updated'],
+				'address': entry['address'],
+				'day': day}
+			for entry in data['entries']
+			]
+	return(dataImp)
 
 def by_price(x): # function handle for sort mutable method or sorted non-mutable function to sort prices in list of dictionaries x
 	return x['price']
@@ -20,12 +29,13 @@ def createfuelHTMLTABLE(data): # creates fuel table with columns Price, Location
 	
 	body = ''
 	
+	colour ="#008080" #blue = "#008080"
 	# loop creating body of table by iterating over each dictionary in list info grabbed f
 	for entry in data:
-		if entry['updated'] == getDate():
-			body = body + '<tr> <td> {pr} </td> <td> {loc} </td> <td>{br}</td> <td>{addr}</td> <td>{dt}</td> </tr>'.format(loc = entry['location'], br = entry['brand'], pr = entry['price'],addr = entry['address'],dt = entry['updated'])
+		if entry['day'] == 'today':
+			body = body + '<tr> <td> {pr} </td> <td> {loc} </td> <td>{br}</td> <td>{addr}</td> <td>{dt}</td> </tr>'.format(loc = entry['location'], br = entry['brand'], pr = entry['price'],addr = entry['address'],dt = entry['date'])
 		else:
-			body = body + '<tr bgcolor ="#008080"> <td> {pr} </td> <td> {loc} </td> <td>{br}</td> <td>{addr}</td> <td>{dt}</td> </tr>'.format(loc = entry['location'], br = entry['brand'], pr = entry['price'],addr = entry['address'],dt = entry['updated'])
+			body = body + '<tr bgcolor = {col}> <td> {pr} </td> <td> {loc} </td> <td>{br}</td> <td>{addr}</td> <td>{dt}</td> </tr>'.format(loc = entry['location'], br = entry['brand'], pr = entry['price'],addr = entry['address'],dt = entry['date'], col = colour)
 	body = '<tbody>'+body+'</tbody>'
 	legend = "Tomorrow's price (blue)"
 	tableF = '<table>' + header + body + '</table>' + '<h1> Legend: </h1>'+'<p> {leg} </p>'.format(leg = legend)
@@ -36,9 +46,9 @@ def writeTable(tableDat,fileName,): #writing function for table
 	with open(fileName,'w') as f:
 		f.write(tableDat)	
 
-def getDate():
-	now = datetime.now()
-	return str(now)[0:10]
+#def getDate():
+#	now = datetime.now()
+#	return str(now)[0:10]
 
 def main():
 	
@@ -50,7 +60,7 @@ def main():
 	
 	fuelInfo = fuelInfoToday + fuelInfoTomorrow 
 	
-
+	pprint(fuelInfo)
 	
 	#fuelInfoToday.sort(key = by_price) #other option is to: fuelInfo = sorted(fuelInfo, key = by_price)
 	#fuelInfoTomorrow.sort(key = by_price) 
@@ -68,3 +78,14 @@ def main():
 
 main() #executing main function nested with other defined functions
 
+#feedback from Robin Chew:
+# use of **dic for unpacking arguments in dictionaries and *l for unpacking arguments in lists. e.g. minus(a,b) let d = {'a': 5, 'b':2} and l = [5, 2] then minus(**d) == minus(b=2,a=5) == minus(5,2) = minus(*l) etc etc
+# create own dictionary
+#
+#
+#
+#
+#
+#
+#
+#
